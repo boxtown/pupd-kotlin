77ccb8df65b4966631cfbb112be4ac8df5eb94f7
Michael Ma <michael60612@gmail.com>
2016-09-14 21:15:00 -0400

    test:

--- data/build/resources/main/reset_db.sh
+++
@@ -1,9 +0,0 @@
-# This script requires PGPASSWORD environment variable to be set to run
-{
-  "${PGPASSWORD?Need to set PGPASSWORD env var}"
-} &> /dev/null
-
-echo "DROP DATABASE pupddb; \q\n" | psql -U postgres
-
-psql -U postgres -f ./sql/create_schema.sql
-psql -U postgres -f ./sql/seed.sql
